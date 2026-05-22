5da73f63a454636b169e663d0639a29896a6a23e
SebastianNeter <sebastian.neter@gmail.com>
2026-05-22 13:59:09 -0300

    feat(plugin): bump version to 2026052201 / release 2.4.0
    Triggers Moodle to detect the new version and re-register webservices,
    including the newly added local_myddleware_get_roc_groups_by_date.

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2026042401;
+$plugin->version  = 2026052201;
 $plugin->requires = 2010112401;
 $plugin->cron     = 0;
-$plugin->release = '2.3.3';
+$plugin->release = '2.4.0';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'local_myddleware';
